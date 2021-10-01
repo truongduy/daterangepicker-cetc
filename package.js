82106a19273939af469302f8d69ae4e0ef645c59
duy.dang <duy.dang@sutrixsolutions.com>
2021-10-01 15:31:02 +0700

    update package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '3.1.0',
+  version: '3.1.1',
   summary: 'Date range picker component',
   git: 'https://github.com/truongduy/daterangepicker-cetc',
   documentation: 'README.md'
